42df947824e1ab4fad65b2b7aaf78c9f82ff3578
car123moving-prog <car123.moving@gmail.com>
2026-01-10 14:02:15 +0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// index.js
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
